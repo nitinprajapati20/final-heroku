dd592c326a571d2e3e9746ce09bda5972a922675
nitinprajapati20 <61464540+nitinprajapati20@users.noreply.github.com>
2020-02-26 19:21:39 +0530

    Add files via upload

---
+++ history.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# *** Spyder Python Console History Log ***
+
+## ---(Sun Feb 23 18:10:14 2020)---
+runfile('C:/Users/admin/.spyder-py3/temp.py', wdir='C:/Users/admin/.spyder-py3')
+runfile('C:/Users/admin/.spyder-py3/request.py', wdir='C:/Users/admin/.spyder-py3')
+runfile('C:/Users/admin/.spyder-py3/app.py', wdir='C:/Users/admin/.spyder-py3')
+runfile('C:/Users/admin/.spyder-py3/temp.py', wdir='C:/Users/admin/.spyder-py3')
+runfile('C:/Users/admin/.spyder-py3/request.py', wdir='C:/Users/admin/.spyder-py3')
+runfile('C:/Users/admin/.spyder-py3/temp.py', wdir='C:/Users/admin/.spyder-py3')
+runfile('C:/Users/admin/.spyder-py3/app.py', wdir='C:/Users/admin/.spyder-py3')
+runfile('C:/Users/admin/.spyder-py3/request.py', wdir='C:/Users/admin/.spyder-py3')
+runfile('C:/Users/admin/.spyder-py3/app.py', wdir='C:/Users/admin/.spyder-py3')
+runfile('C:/Users/admin/.spyder-py3/request.py', wdir='C:/Users/admin/.spyder-py3')
+runfile('C:/Users/admin/.spyder-py3/app.py', wdir='C:/Users/admin/.spyder-py3')
+runfile('C:/Users/admin/.spyder-py3/request.py', wdir='C:/Users/admin/.spyder-py3')
+runfile('C:/Users/admin/.spyder-py3/app.py', wdir='C:/Users/admin/.spyder-py3')
+runfile('C:/Users/admin/.spyder-py3/request.py', wdir='C:/Users/admin/.spyder-py3')
+
+## ---(Tue Feb 25 13:56:41 2020)---
+runfile('C:/Users/admin/.spyder-py3/temp.py', wdir='C:/Users/admin/.spyder-py3')
+runfile('C:/Users/admin/.spyder-py3/app.py', wdir='C:/Users/admin/.spyder-py3')
+
+## ---(Wed Feb 26 13:13:59 2020)---
+runfile('C:/Users/admin/.spyder-py3/temp.py', wdir='C:/Users/admin/.spyder-py3')
+runfile('C:/Users/admin/.spyder-py3/request.py', wdir='C:/Users/admin/.spyder-py3')
